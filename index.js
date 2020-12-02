421aa4beda0338865220441a75f1d8ef3a9df30f
Kos Krzysztof <krzysztof.kos33@gmail.com>
2020-12-02 09:46:49 +0100

    change styles

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
 import {tableData} from "./data/data.js";
 
 function tableCreate(data) {
-  
 
-    // * GET TABLE CONTAINER ELEMENT
     const tableContainer = document.querySelector('.dynamic-table');
 
-    // * CREATE BASIC TABLE ELEMENTS
     const table = document.createElement('table');
     const tableRow = document.createElement('tr');
 
     tableContainer.appendChild(table);
     table.appendChild(tableRow);
 
-      // * CREATE TH
-    // * GET OBJECT KEYS
     let keys=[];
     data.forEach(element => {
         keys = Object.keys(element);
     });
-    console.log(keys);
    
-    // * CREATE TABLE HEADER
     keys.forEach(key => {
         const tableHeaderCell = document.createElement('th');
         tableHeaderCell.innerHTML = key;
         tableRow.appendChild(tableHeaderCell);
     });
 
-    // * CREATE TABLE BODY
-    // * GET OBJECT VALUE
     data.map(object => { 
-
-        // * CREATE ROWS
         const tableRow = document.createElement('tr');
         table.appendChild(tableRow);
-
-        // * GET OBJECT VALUE
+        
         const objectValues = Object.values(object); 
         objectValues.forEach(value => {
            
